c9dee4e8ccc187acf90329ce40db674a8af42a43
benko-dev <dzubenkod7@gmail.com>
2024-05-19 15:02:29 +0200

    fresh

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,13 +1,33 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
+const customAuthMiddleware = async (req, event) => {
+  // Стандартная обработка с помощью authMiddleware
+  const response = await authMiddleware({
+    publicRoutes: ["/sign-in", "/sign-up"],
+  })(req, event);
 
-export default authMiddleware({
-    publicRoutes: [
-        "/sign-in",
-        "/sign-up"
-    ]
-})
+  // Ваша кастомная логика
+  if (!response) {
+    const { user } = req.auth;
+    if (!user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    return new Response(`Hello, ${user.firstName}`);
+  }
+
+  return response;
+};
+
+// Экспортируем как default экспорт
+export default customAuthMiddleware;
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/order", "/order/:path*", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+.[w]+$|_next).*)",
+    "/",
+    "/order",
+    "/order/:path*",
+    "/(api|trpc)(.*)",
+  ],
 };
